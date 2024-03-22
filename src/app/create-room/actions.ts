4396f0952bcaf8f0b78129c6ae992db89d6bc0f0
aayush <sharma31aayush@gmail.com>
2024-03-22 17:02:45 +0530

    add: display rooms on ui

--- src/app/create-room/actions.ts
+++ src/app/create-room/actions.ts
@@ -7,6 +7,9 @@ export async function createRoomAction(
   roomData: Omit<Room, 'id' | 'userId'>,
   userId: string | null | undefined //doing this because the userID coming from clerk has string | null | undefined
 ) {
+  if (!userId) {
+    throw new Error('User not found');
+  }
   //the user id entered here should be same as user.id in the users table
   await db.insert(room).values({ ...roomData, userId: userId });
 }
