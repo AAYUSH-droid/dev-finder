59f2e93911a53ab9f30c7c225ed00f5f485c298f
aayush <sharma31aayush@gmail.com>
2024-03-26 16:24:14 +0530

    style: room/id page styling

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -1,13 +1,51 @@
 import { getRoom } from '@/data-access/rooms';
+import { GithubIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default async function RoomPage(props: { params: { roomId: string } }) {
   const roomID = props.params.roomId;
   const room = await getRoom(roomID);
+
+  if (!room) {
+    return <div>Room not found</div>;
+  }
   return (
-    <div>
-      <div>
-        {/* <h1>Room ID: {roomID}</h1> */}
-        <h2>{room?.name}</h2>
+    // <div>
+    //   <div className='grid grid-cols-4 min-h-screen'>
+    //     <div className='col-span-3 bg-blue-400'>VIDEO PLAYER</div>
+    //     <div className='bg-red-500'>INFO PANEL{room?.name}</div>
+    //   </div>
+    // </div>
+    <div className='grid grid-cols-4 min-h-screen'>
+      <div className='col-span-3 p-4 pr-2'>
+        {/* <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 min-h-screen">
+        <DevFinderVideo room={room} />
+      </div> */}
+        <div className='rounded-lg border bg-card text-card-foreground shadow-sm p-4 min-h-screen'>
+          VIDEO PLAYER
+        </div>
+      </div>
+
+      <div className='col-span-1 p-4 pl-2'>
+        <div className='rounded-lg border bg-card text-card-foreground shadow-sm p-4 flex flex-col gap-4'>
+          <h1 className='text-base'>{room?.name}</h1>
+
+          {room.githubRepo && (
+            <Link
+              href={room.githubRepo}
+              className='flex items-center gap-2 text-center text-sm'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <GithubIcon />
+              Github Project
+            </Link>
+          )}
+
+          <p className='text-base text-gray-600'>{room?.description}</p>
+
+          {/* <TagsList tags={splitTags(room.tags)} /> */}
+        </div>
       </div>
     </div>
   );
